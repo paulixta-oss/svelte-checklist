6c40b3a949d99ed42a4a11b465bef4ba2158a92c
Rodrigo Couto <r@rodg.co>
2021-03-22 01:23:54 -0300

    feat: :seedling: added examples of custom Header and Item in the samples folder

---
+++ samples/HeaderCountTasks.svelte
@@ -0,0 +1,14 @@
+<script>
+  export let checklist;
+
+  $: togo = $checklist.data.length - $checklist.selected.length;
+</script>
+
+{#if $checklist.allChecked}
+  <p>Congratulations you've completed all your tasks</p>
+{:else}
+  <p>{togo} {togo === 1 ? "task" : "tasks"} to go!</p>
+  <button type="button" on:click={() => checklist.checkAll()}
+    >Complete All
+  </button>
+{/if}
---
+++ samples/ItemCrosThrough.svelte
@@ -0,0 +1,15 @@
+<script>
+  export let value = "";
+  export let checked = false;
+</script>
+
+<p on:click={() => (checked = !checked)} class:checked>{value}</p>
+
+<style>
+  p {
+    cursor: pointer;
+  }
+  .checked {
+    text-decoration: line-through;
+  }
+</style>
