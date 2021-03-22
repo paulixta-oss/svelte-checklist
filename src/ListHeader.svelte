0e6438123b607b19d7e33529e049d9fa0f8a3c04
Rodrigo Couto <r@rodg.co>
2021-03-22 10:21:36 -0300

    refactor: :fire: simplified the default header

--- src/ListHeader.svelte
+++ src/ListHeader.svelte
@@ -5,23 +5,15 @@
 
   let mainCheckbox;
   let newItem = "";
-  let showMenu = false;
   let options;
 
   onMount(() => {
     checklist.addCallback("delete", (id) => checklist.remove(id));
   });
 
-  $: if ($checklist) options = [...checklist.options()];
   $: if (mainCheckbox) mainCheckbox.checked = $checklist.allChecked;
   $: if (mainCheckbox) mainCheckbox.indeterminate = $checklist.someChecked;
 </script>
-
-<svelte:window
-  on:click={() => {
-    showMenu = false;
-  }}
-/>
 
 <div>
   <input type="text" bind:value={newItem} />
@@ -35,33 +27,13 @@
   >
 </div>
 
-<div class="dropdown">
-  <div class="checkbox show">
-    <input
-      name="mainCheck"
-      type="checkbox"
-      bind:this={mainCheckbox}
-      on:click={() => checklist.setAll(mainCheckbox.checked)}
-    />
-  </div>
-  <div class="select">
-    <button
-      on:click|stopPropagation={() => {
-        showMenu = true;
-      }}
-      class="dropbtn">▼</button
-    >
-  </div>
-
-  <div class="dropdown-content" class:show={showMenu}>
-    <span on:click={() => checklist.checkAll()}>All</span>
-    <span on:click={() => checklist.uncheckAll()}>None</span>
-    <span on:click={() => checklist.toggleAll()}>Toggle</span>
-    {#each options as option}
-      <span on:click={() => checklist.checkOnly(option)}>{option}</span>
-      <span on:click={() => checklist.checkPlus(option)}>+{option}</span>
-    {/each}
-  </div>
+<div class="checkbox show">
+  <input
+    name="mainCheck"
+    type="checkbox"
+    bind:this={mainCheckbox}
+    on:click={() => checklist.setAll(mainCheckbox.checked)}
+  />
 </div>
 
 <slot />
@@ -82,29 +54,7 @@
     border-radius: 4px;
   }
 
-  .dropbtn {
-    color: black;
-    font-size: 1em;
-    border: none;
-    cursor: pointer;
-    background-color: inherit;
-    width: 100%;
-    height: 100%;
-  }
-
-  .dropbtn:hover,
-  .dropbtn:focus,
-  .dropbtn:active {
-    background-color: #ddd;
-  }
-
-  .dropdown {
-    position: relative;
-    display: inline-grid;
-    grid-template-columns: repeat(2, min-content);
-  }
-
-  input,
+  input[type="checkbox"],
   button {
     margin: 0;
     padding: 0;
@@ -127,41 +77,6 @@
   .checkbox:hover,
   .checkbox:focus,
   .checkbox:active {
-    background-color: #ddd;
-  }
-
-  .select {
-    height: 30px;
-    width: 20px;
-  }
-
-  .select:hover,
-  .select:focus,
-  .select:active {
-    background-color: #ddd;
-  }
-
-  .dropdown-content {
-    display: none;
-    position: absolute;
-    background-color: #f1f1f1;
-    min-width: 160px;
-    overflow: auto;
-    box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
-    z-index: 1;
-    margin-top: 30px;
-  }
-
-  .dropdown-content span {
-    color: black;
-    padding: 12px 16px;
-    text-decoration: none;
-    display: block;
-    background-color: inherit;
-    height: 100%;
-  }
-
-  .dropdown span:hover {
     background-color: #ddd;
   }
 
