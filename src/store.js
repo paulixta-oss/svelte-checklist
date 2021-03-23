7c7d9a34eaed4bfabb232ebbe1c206e7b6e78a61
Rodrigo Couto <r@rodg.co>
2021-03-22 22:30:29 -0300

    feat: :sparkles: implemented custom store
    got rid of writable and implemented a copy of it as a custom store

--- src/store.js
+++ src/store.js
@@ -1,7 +1,9 @@
-import { writable } from "svelte/store";
+import { noop, safe_not_equal } from "svelte/internal";
 
-export default function (data = []) {
-  const value = {
+const subscriber_queue = [];
+
+export default function (data = [], start = noop) {
+  let value = {
     data: data.map((v) => ({
       id: (new Date().getTime() + Math.random()).toString(36),
       value: v,
@@ -15,7 +17,57 @@ export default function (data = []) {
 
   let callbacks = {};
 
-  let { subscribe, update, set } = writable(value);
+  let stop;
+  const subscribers = [];
+
+  function set(new_value) {
+    if (safe_not_equal(value, new_value)) {
+      value = new_value;
+      value.allChecked = value.data.map((e) => e.checked).every((v) => v);
+      value.noneChecked = value.data.map((e) => e.checked).every((v) => !v);
+      value.someChecked = !value.allChecked && !value.noneChecked;
+      value.selected = value.data.filter((e) => e.checked).map((e) => e.value);
+    }
+    if (stop) {
+      // store is ready
+      const run_queue = !subscriber_queue.length;
+      for (let i = 0; i < subscribers.length; i += 1) {
+        const s = subscribers[i];
+        s[1]();
+        subscriber_queue.push(s, value);
+      }
+      if (run_queue) {
+        for (let i = 0; i < subscriber_queue.length; i += 2) {
+          subscriber_queue[i][0](subscriber_queue[i + 1]);
+        }
+        subscriber_queue.length = 0;
+      }
+    }
+  }
+
+  function update(fn) {
+    set(fn(value));
+  }
+
+  function subscribe(run, invalidate = noop) {
+    const subscriber = [run, invalidate];
+    subscribers.push(subscriber);
+    if (subscribers.length === 1) {
+      stop = start(set) || noop;
+    }
+    run(value);
+
+    return () => {
+      const index = subscribers.indexOf(subscriber);
+      if (index !== -1) {
+        subscribers.splice(index, 1);
+      }
+      if (subscribers.length === 0) {
+        stop();
+        stop = null;
+      }
+    };
+  }
 
   const methods = {
     checkStates() {
